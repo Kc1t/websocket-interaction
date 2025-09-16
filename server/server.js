5ead75946f347cd6073e09146f94e58124609a2f
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2025-09-16 18:57:37 -0300

    Atualizar o server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
